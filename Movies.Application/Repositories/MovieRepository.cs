caf1565fc48e33cd4019da37002ab9dee52b148c
Sergei Anikeev <sergei.anikeev@spark.co.nz>
2024-12-19 13:37:44 +1300

    add more files

--- Movies.Application/Repositories/MovieRepository.cs
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Movies.Application.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,29 @@ namespace Movies.Application.Repositories
 {
     internal class MovieRepository : IMovieRepository
     {
+        Task<bool> IMovieRepository.CreateAsync(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IMovieRepository.DeleteByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<Movie>> IMovieRepository.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Movie?> IMovieRepository.GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IMovieRepository.UpdateAsync(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
