eb0ba08f4d967b2ca21ad855ee4c058d1bf9276e
Sergei Anikeev <anikeevsu@gmail.com>
2025-03-05 16:17:38 +1300

    Refit

---
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,9 @@
+﻿using Refit;
+using Movies.Api.Sdk;
+using System.Text.Json;
+
+var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+
+var movie = await moviesApi.GetMovieAsync("movie-1-2023");
+
+Console.WriteLine(JsonSerializer.Serialize(movie));
