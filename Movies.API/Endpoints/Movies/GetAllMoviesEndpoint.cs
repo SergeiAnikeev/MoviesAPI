2fc78383facb95fcc637598e76e72244987d1eb9
Sergei Anikeev <anikeevsu@gmail.com>
2025-03-13 10:55:09 +1300

    versioning

--- Movies.API/Endpoints/Movies/GetAllMoviesEndpoint.cs
+++ Movies.API/Endpoints/Movies/GetAllMoviesEndpoint.cs
@@ -32,8 +32,37 @@ namespace Movies.API.Endpoints.Movies
                     request.PageSize.GetValueOrDefault(PagedRequest.DefaultPageSize), 
                     movieCount));
             })
-                .WithName(Name)
-                .Produces<MoviesResponse>(StatusCodes.Status200OK);
+                .WithName($"{Name}V1")
+                .Produces<MoviesResponse>(StatusCodes.Status200OK)
+                .WithApiVersionSet(ApiVersioning.VersionSet)
+                .HasApiVersion(1.0);
+
+
+            app.MapGet(ApiEndpoints.Movies.GetAll, async
+               ([AsParameters] GetAllMoviesRequest request,
+               HttpContext context,
+               IMovieService movieService,
+               CancellationToken token) =>
+            {
+                var userId = context.GetUserId();
+                var options = request.MapToOptions().WithUser(userId);
+                var movies = await movieService.GetAllAsync(options, token);
+                var movieCount = await movieService.GetCountAsync(options.Title, options.YearOfRelease, token);
+                if (movies is null)
+                {
+                    return Results.NotFound();
+                }
+
+                return TypedResults.Ok(movies.MapToResponse(
+                    request.Page.GetValueOrDefault(PagedRequest.DefaultPage),
+                    request.PageSize.GetValueOrDefault(PagedRequest.DefaultPageSize),
+                    movieCount));
+            })
+               .WithName($"{Name}V2")
+               .Produces<MoviesResponse>(StatusCodes.Status200OK)
+               .WithApiVersionSet(ApiVersioning.VersionSet)
+               .HasApiVersion(2.0);
+
 
             return app;
         }
